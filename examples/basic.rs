71579afed9ee9502698e944dbd756301c6614450
Laurent <laurent.mazare@gmail.com>
2024-09-07 18:59:40 +0200

    Simple matrix type.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,6 +1,5 @@
 fn main() -> anyhow::Result<()> {
     objc::rc::autoreleasepool(|| {
-        gemm_metal::dot_product()?;
         for sz in [512, 1024, 2048, 4096, 4096 + 2048, 8192] {
             gemm_metal::gemm_naive(sz, sz, sz, 100)?;
             gemm_metal::gemm_coalescing(sz, sz, sz, 100)?;
--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,48 @@
+use anyhow::Result;
+
 const DOT: &str = include_str!("dot_product.metal");
 
 mod utils;
 
-fn read_to_vec<T: Clone>(buffer: &metal::Buffer, n: usize) -> Vec<T> {
-    let ptr = buffer.contents() as *const T;
-    assert!(!ptr.is_null());
-    let slice = unsafe { std::slice::from_raw_parts(ptr, n) };
-    slice.to_vec()
+pub struct Matrix<T> {
+    buffer: metal::Buffer,
+    _phantom: std::marker::PhantomData<T>,
+    rows: usize,
+    cols: usize,
 }
 
-fn new_buffer<T>(device: &metal::Device, data: &[T]) -> metal::Buffer {
-    let options = metal::MTLResourceOptions::StorageModeManaged;
-    let ptr = data.as_ptr() as *const std::ffi::c_void;
-    let size = std::mem::size_of_val(data) as u64;
-    device.new_buffer_with_data(ptr, size, options)
+impl<T> Matrix<T> {
+    fn numels(&self) -> usize {
+        self.rows * self.cols
+    }
 }
 
-pub fn dot_product() -> anyhow::Result<()> {
-    use metal::Device;
+impl<T: Clone> Matrix<T> {
+    fn zeros(device: &metal::Device, rows: usize, cols: usize) -> Self {
+        let options = metal::MTLResourceOptions::StorageModeManaged;
+        let bytes_len = (rows * cols * std::mem::size_of::<T>()) as u64;
+        let buffer = device.new_buffer(bytes_len, options);
+        Self { buffer, _phantom: std::marker::PhantomData, rows, cols }
+    }
 
-    println!("hello world");
-    let device = Device::system_default().expect("No device found");
-    let lib = device.new_library_with_source(DOT, &metal::CompileOptions::new()).unwrap();
-    let function = lib.get_function("dot_product", None).unwrap();
-    let pipeline = device.new_compute_pipeline_state_with_function(&function).unwrap();
-    let a = new_buffer(&device, &[42f32, 2.0, 3., 3., 7., 9.9999]);
-    let b = new_buffer(&device, &[1f32, -3.14, 3., 7., 33., 9.9999]);
+    fn new(device: &metal::Device, data: &[T], rows: usize, cols: usize) -> Result<Self> {
+        let options = metal::MTLResourceOptions::StorageModeManaged;
+        let ptr = data.as_ptr() as *const std::ffi::c_void;
+        let size = std::mem::size_of_val(data) as u64;
+        let buffer = device.new_buffer_with_data(ptr, size, options);
+        Ok(Self { buffer, _phantom: std::marker::PhantomData, rows, cols })
+    }
 
-    let length = a.length() / std::mem::size_of::<u32>() as u64;
-    let size = length * core::mem::size_of::<u32>() as u64;
+    fn to_vec(&self) -> Vec<T> {
+        let ptr = self.buffer.contents() as *const T;
+        assert!(!ptr.is_null());
+        let slice = unsafe { std::slice::from_raw_parts(ptr, self.numels()) };
+        slice.to_vec()
+    }
 
-    let c = device.new_buffer(size, metal::MTLResourceOptions::StorageModeManaged);
-    let command_queue = device.new_command_queue();
-    let cb = command_queue.new_command_buffer();
-
-    let encoder = cb.new_compute_command_encoder();
-    encoder.set_compute_pipeline_state(&pipeline);
-    set_params!(encoder, (&a, &b, &c));
-    encoder.use_resource(&a, metal::MTLResourceUsage::Read);
-    encoder.use_resource(&b, metal::MTLResourceUsage::Read);
-    encoder.use_resource(&c, metal::MTLResourceUsage::Write);
-    let grid_size = metal::MTLSize::new(length, 1, 1);
-    let threadgroup_size = metal::MTLSize::new(1, 1, 1);
-    encoder.dispatch_threads(grid_size, threadgroup_size);
-    encoder.end_encoding();
-    cb.commit();
-    cb.wait_until_completed();
-    println!("Done!");
-    let v: Vec<f32> = read_to_vec(&c, length as usize);
-    println!("{v:?}");
-
-    Ok(())
+    fn buffer(&self) -> &metal::Buffer {
+        &self.buffer
+    }
 }
 
 pub fn gemm_naive(m: usize, n: usize, k: usize, repeats: usize) -> anyhow::Result<()> {
@@ -60,23 +52,20 @@ pub fn gemm_naive(m: usize, n: usize, k: usize, repeats: usize) -> anyhow::Resul
     let lib = device.new_library_with_source(DOT, &metal::CompileOptions::new()).unwrap();
     let function = lib.get_function("sgemm_naive", None).unwrap();
     let pipeline = device.new_compute_pipeline_state_with_function(&function).unwrap();
-    let a = new_buffer(&device, &vec![0f32; m * k]);
-    let b = new_buffer(&device, &vec![0f32; k * n]);
-
-    let c = device.new_buffer(
-        (m * n * std::mem::size_of::<f32>()) as u64,
-        metal::MTLResourceOptions::StorageModeManaged,
-    );
+    let a: Matrix<f32> = Matrix::zeros(&device, m, k);
+    let b: Matrix<f32> = Matrix::zeros(&device, k, n);
+    let c: Matrix<f32> = Matrix::zeros(&device, m, n);
     let command_queue = device.new_command_queue();
     let start_time = std::time::Instant::now();
     for _ in 0..repeats {
+        let (a, b, c) = (a.buffer(), b.buffer(), c.buffer());
         let cb = command_queue.new_command_buffer();
         let encoder = cb.new_compute_command_encoder();
         encoder.set_compute_pipeline_state(&pipeline);
-        set_params!(encoder, (&a, &b, &c, m, n, k, 1., 0.));
-        encoder.use_resource(&a, metal::MTLResourceUsage::Read);
-        encoder.use_resource(&b, metal::MTLResourceUsage::Read);
-        encoder.use_resource(&c, metal::MTLResourceUsage::Write);
+        set_params!(encoder, (a, b, c, m, n, k, 1., 0.));
+        encoder.use_resource(a, metal::MTLResourceUsage::Read);
+        encoder.use_resource(b, metal::MTLResourceUsage::Read);
+        encoder.use_resource(c, metal::MTLResourceUsage::Write);
         let grid_size = metal::MTLSize::new(m.div_ceil(32) as u64, n.div_ceil(32) as u64, 1);
         let threadgroup_size = metal::MTLSize::new(32, 32, 1);
         encoder.dispatch_threads(grid_size, threadgroup_size);
@@ -97,23 +86,20 @@ pub fn gemm_coalescing(m: usize, n: usize, k: usize, repeats: usize) -> anyhow::
     let lib = device.new_library_with_source(DOT, &metal::CompileOptions::new()).unwrap();
     let function = lib.get_function("sgemm_coalescing", None).unwrap();
     let pipeline = device.new_compute_pipeline_state_with_function(&function).unwrap();
-    let a = new_buffer(&device, &vec![0f32; m * k]);
-    let b = new_buffer(&device, &vec![0f32; k * n]);
-
-    let c = device.new_buffer(
-        (m * n * std::mem::size_of::<f32>()) as u64,
-        metal::MTLResourceOptions::StorageModeManaged,
-    );
+    let a: Matrix<f32> = Matrix::zeros(&device, m, k);
+    let b: Matrix<f32> = Matrix::zeros(&device, k, n);
+    let c: Matrix<f32> = Matrix::zeros(&device, m, n);
     let command_queue = device.new_command_queue();
     let start_time = std::time::Instant::now();
     for _ in 0..repeats {
+        let (a, b, c) = (a.buffer(), b.buffer(), c.buffer());
         let cb = command_queue.new_command_buffer();
         let encoder = cb.new_compute_command_encoder();
         encoder.set_compute_pipeline_state(&pipeline);
-        set_params!(encoder, (&a, &b, &c, m, n, k, 1., 0.));
-        encoder.use_resource(&a, metal::MTLResourceUsage::Read);
-        encoder.use_resource(&b, metal::MTLResourceUsage::Read);
-        encoder.use_resource(&c, metal::MTLResourceUsage::Write);
+        set_params!(encoder, (a, b, c, m, n, k, 1., 0.));
+        encoder.use_resource(a, metal::MTLResourceUsage::Read);
+        encoder.use_resource(b, metal::MTLResourceUsage::Read);
+        encoder.use_resource(c, metal::MTLResourceUsage::Write);
         let grid_size = metal::MTLSize::new(m.div_ceil(32) as u64, n.div_ceil(32) as u64, 1);
         let threadgroup_size = metal::MTLSize::new(32 * 32, 1, 1);
         encoder.dispatch_threads(grid_size, threadgroup_size);
