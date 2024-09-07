7d93d93f32694400b40e7e4686e3a1a486e7151e
Laurent <laurent.mazare@gmail.com>
2024-09-07 16:50:23 +0200

    More naive sgemm.

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,7 +1,9 @@
 fn main() -> anyhow::Result<()> {
     objc::rc::autoreleasepool(|| {
         gemm_metal::dot_product()?;
-        gemm_metal::gemm_naive(1024, 1024)?;
+        for sz in [512, 1024, 2048, 4096, 8192] {
+            gemm_metal::gemm_naive(sz, sz, sz, 100)?;
+        }
         Ok(())
     })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -53,38 +53,39 @@ pub fn dot_product() -> anyhow::Result<()> {
     Ok(())
 }
 
-pub fn gemm_naive(n: usize, m: usize) -> anyhow::Result<()> {
+pub fn gemm_naive(m: usize, n: usize, k: usize, repeats: usize) -> anyhow::Result<()> {
     use metal::Device;
 
     let device = Device::system_default().expect("No device found");
     let lib = device.new_library_with_source(DOT, &metal::CompileOptions::new()).unwrap();
     let function = lib.get_function("sgemm_naive", None).unwrap();
     let pipeline = device.new_compute_pipeline_state_with_function(&function).unwrap();
-    let a = new_buffer(&device, &[42f32, 2.0, 3., 3.]);
-    let b = new_buffer(&device, &[1f32, -3.14, 3., 7.]);
+    let a = new_buffer(&device, &vec![0f32; m * k]);
+    let b = new_buffer(&device, &vec![0f32; k * n]);
 
-    let length = a.length() / std::mem::size_of::<u32>() as u64;
-    let size = length * core::mem::size_of::<u32>() as u64;
-
-    let c = device.new_buffer(size, metal::MTLResourceOptions::StorageModeManaged);
+    let c = device.new_buffer(
+        (m * n * std::mem::size_of::<f32>()) as u64,
+        metal::MTLResourceOptions::StorageModeManaged,
+    );
     let command_queue = device.new_command_queue();
-    let cb = command_queue.new_command_buffer();
-
-    let encoder = cb.new_compute_command_encoder();
-    encoder.set_compute_pipeline_state(&pipeline);
-    set_params!(encoder, (&a, &b, &c, 2, 2, 2, 1., 0.));
-    encoder.use_resource(&a, metal::MTLResourceUsage::Read);
-    encoder.use_resource(&b, metal::MTLResourceUsage::Read);
-    encoder.use_resource(&c, metal::MTLResourceUsage::Write);
-    let grid_size = metal::MTLSize::new(m.div_ceil(32) as u64, n.div_ceil(32) as u64, 1);
-    let threadgroup_size = metal::MTLSize::new(32, 32, 1);
-    encoder.dispatch_threads(grid_size, threadgroup_size);
-    encoder.end_encoding();
-    cb.commit();
-    cb.wait_until_completed();
-    println!("Done!");
-    let v: Vec<f32> = read_to_vec(&c, length as usize);
-    println!("{v:?}");
-
+    let start_time = std::time::Instant::now();
+    for _ in 0..repeats {
+        let cb = command_queue.new_command_buffer();
+        let encoder = cb.new_compute_command_encoder();
+        encoder.set_compute_pipeline_state(&pipeline);
+        set_params!(encoder, (&a, &b, &c, m, n, k, 1., 0.));
+        encoder.use_resource(&a, metal::MTLResourceUsage::Read);
+        encoder.use_resource(&b, metal::MTLResourceUsage::Read);
+        encoder.use_resource(&c, metal::MTLResourceUsage::Write);
+        let grid_size = metal::MTLSize::new(m.div_ceil(32) as u64, n.div_ceil(32) as u64, 1);
+        let threadgroup_size = metal::MTLSize::new(32, 32, 1);
+        encoder.dispatch_threads(grid_size, threadgroup_size);
+        encoder.end_encoding();
+        cb.commit();
+        cb.wait_until_completed();
+    }
+    let dt = start_time.elapsed().as_secs_f64() / repeats as f64;
+    let gflops = (n * m * k) as f64 * 2. / (1e9 * dt);
+    println!("{m} {n} {k} {gflops:.2}");
     Ok(())
 }
